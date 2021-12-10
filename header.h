d53439dd6b33866bb5f46c33f2bac9661d08ce21
Ahmed-Sedeek <sedeekas@azhar.edu.eg>
2021-12-10 09:33:41 +0200

    Add CallBack Function

--- header.h
+++ header.h
@@ -14,6 +14,23 @@
  *
 */
 
+/*---------------------- Function Macros ----------------------*/
+/*Pins Utils*/
+#define  SET_BIT(REGISTER, BIT_POS)              (REGISTER |= (PIN_MASK_VAL<<BIT_POS))
+#define  CLEAR_BIT(REGISTER, BIT_POS)            (REGISTER &= ~(PIN_MASK_VAL<<BIT_POS))
+#define  TOGGLE_BIT(REGISTER, BIT_POS)           (REGISTER ^= (PIN_MASK_VAL<<BIT_POS))
+#define  READ_BIT(REGISTER, BIT_POS)             ((REGISTER >> BIT_POS) & PIN_MASK_VAL)
+#define  WRITE_BIT(REGISTER, BIT_POS, VALUE)     ((REGISTER &= ~(PIN_MASK_VAL<<BIT_POS))|(VALUE << BIT_POS))
+
+/*Port Utils*/
+#define SET_PORT(REGISTER)                 (REGISTER |= PORT_MASK_VALUE)
+#define CLEAR_PORT(REGISTER)               (REGISTER &= ~PORT_MASK_VALUE)
+#define TOGGLE_PORT(REGISTER)              (REGISTER ^= PORT_MASK_VALUE)
+#define READ_PORT(REGISTER)                (REGISTER &= PORT_MASK_VALUE)
+#define WRITE_PORT(REGISTER, VALUE)        ((REGISTER &= ~PORT_MASK_VALUE)|VALUE)
+
+
+
 #define TRUE   1
 #define FALSE  0
 
--- main.c
+++ main.c
@@ -8,32 +8,21 @@
 #define  PIN_MASK_VAL	      1U
 #define  PORT_MASK_VALUE      0XFF
 
-/*---------------------- Function Macros ----------------------*/
-/*Pins Utils*/
-#define  SET_BIT(REGISTER, BIT_POS)              (REGISTER |= (PIN_MASK_VAL<<BIT_POS))
-#define  CLEAR_BIT(REGISTER, BIT_POS)            (REGISTER &= ~(PIN_MASK_VAL<<BIT_POS))
-#define  TOGGLE_BIT(REGISTER, BIT_POS)           (REGISTER ^= (PIN_MASK_VAL<<BIT_POS))
-#define  READ_BIT(REGISTER, BIT_POS)             ((REGISTER >> BIT_POS) & PIN_MASK_VAL)
-#define  WRITE_BIT(REGISTER, BIT_POS, VALUE)     ((REGISTER &= ~(PIN_MASK_VAL<<BIT_POS))|(VALUE << BIT_POS))
-
-/*Port Utils*/
-#define SET_PORT(REGISTER)                 (REGISTER |= PORT_MASK_VALUE)
-#define CLEAR_PORT(REGISTER)               (REGISTER &= ~PORT_MASK_VALUE)
-#define TOGGLE_PORT(REGISTER)              (REGISTER ^= PORT_MASK_VALUE)
-#define READ_PORT(REGISTER)                (REGISTER &= PORT_MASK_VALUE)
-#define WRITE_PORT(REGISTER, VALUE)        ((REGISTER &= ~PORT_MASK_VALUE)|VALUE)
 
 
 #include <math.h>
 unsigned int (*ptr) (void) = NULL;
 unsigned int (*arrptr[2]) (void) = {NULL, NULL}; // array of pointers to functions
 
-unsigned int *addone(void);
-unsigned int *retArr(void);
+unsigned int *addone(void); // Pointer to function
+unsigned int *retArr(void); // Pointer to function
 
 int getAdd(int num1, int num2);
 int getSub(int num1, int num2);
-int perform_arithmatic_operation(int num1, int num2, int (*funPtr) (int num1, int num2)); // callBack
+int perform_arithmatic_operation(int num1,
+                                 int num2,
+                                 int (*funPtr) (int num1, int num2)
+                                 ); // callBack function
 
 int main(void)
 {
